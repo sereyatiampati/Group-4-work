d8f2c018758e22a18d2762f2c6a1a38d68ca10b2
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-16 11:42:35 +0300

    added a method custom render

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,14 +1,13 @@
 class AnimalsController < ApplicationController
     def index
-        render json: Animal.all
+        render json: Animal.all,  except: [:created_at, :updated_at], methods: [:info]
     end
     def show
         animal=Animal.find_by(id: params[:id])
-        render json: animal
+        render json: animal, methods: [:info],  except: [:created_at, :updated_at]
     end
 
     def create
-
         animal= Animal.create(animal_params)
         render json: animal
     end
@@ -16,7 +15,8 @@ class AnimalsController < ApplicationController
     def update
         animal=Animal.find_by(id: params[:id])
         animal.update(animal_params)
-        render json: animal
+        render json: animal,  except: [:created_at, :updated_at], methods: :info
+
     end
 
     def destroy
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,8 @@ class Animal < ApplicationRecord
     validates :name, presence:true
     validates :age, presence:true, length: {minimum:1}
     validates :sound, presence:true
+
+    def info
+       "#{name}: #{age}years" 
+    end
 end
